e912f04132cb8b500336f3cb13bf337224023c62
Tommy Gomez <tjk2132@columbia.edu>
2022-06-03 15:15:48 -0700

    refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wifi.h>
 #include <WiFiClientSecure.h>
+#include <string.h>
 
 #define NPAGES 5
 
@@ -8,6 +9,8 @@ typedef struct {
 	const char train;
 	const char* stop_id;
 	const char* name;
+	std::string uptown[2];
+	std::string downtown[2];
 } display_page;
 
 const char ssid[] = "zemog";
@@ -35,17 +38,57 @@ const char ca_cert[] = "-----BEGIN CERTIFICATE-----\n" \
 const char server[] = "bcns2d5cx6.execute-api.us-west-1.amazonaws.com";
 
 WiFiClientSecure client;
-int bytes_read;
-int ptr = 0;
-char buf[4095];
+int ptr;
 
 display_page pages[NPAGES] = {
-	{'A', "A24", "59 street"},
-	{'C', "A24", "59 street"},
-	{'E', "A24", "59 street"},
-	{'1', "125", "59 street"},
-	{'E', "R14", "57 street"}
+	{'A', "A24", "59 street", {}, {}},
+	{'C', "A24", "59 street", {}, {}},
+	{'E', "A24", "59 street", {}, {}},
+	{'1', "125", "59 street", {}, {}},
+	{'E', "R14", "57 street", {}, {}}
 };
+
+int make_request(const display_page& page) {
+	Serial.printf("[REQUEST] Requesting times for %c trains at %s\n", 
+			page.train, page.name);
+	if (client.connect(server, 443)) {
+		client.printf("GET /fetch?train=%c&stop-id=%s&n-results=2 HTTP/1.0\r\n",
+				page.train, page.stop_id);
+		client.printf("Host: %s\r\n", server);
+		client.println();
+		return 0;
+	}
+	return 1;
+}
+
+int read_response(display_page& page) {
+	int bytes_read = 0;
+	char buf[4095];
+
+	// Spin until the client has responded.
+	// Requests for 1, 2, 3, 4, 5, 6, 7, and S trains take a while.
+	// TODO: Bug test this for infinite loops.
+	while (!client.available()) {
+		delay(100);
+		if (!client.connected()) {
+			Serial.println("[RESPONSE] Connection ended unexpectedly");
+			client.stop();
+			return 1;
+		}
+	}
+
+	// Read the response into a buffer.
+	while (client.available() && bytes_read < sizeof(buf)) {
+		buf[bytes_read++] = client.read();
+	}
+
+	buf[bytes_read - 1] = 0;
+	client.stop();
+	Serial.printf("[RESPONSE]\n%s\n", buf);
+
+	std::string response(buf);
+	return 0;
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -58,40 +101,23 @@ void setup() {
 	}
 	Serial.printf("[WIFI] Connected to %s\n", ssid);
 	client.setCACert(ca_cert);
+	ptr = 0;
 }
 
 void loop() {
 	delay(1000);
-	bytes_read = 0;
+	display_page& page = pages[ptr];
+	ptr = (++ptr) % NPAGES;
 
-	display_page* page = &pages[ptr++];
-
-	Serial.println("[DATA] Requesting data for ACE trains from MTA's API");
-	if (client.connect(server, 443)) {
-		Serial.println("[DATA] Connected to server");
-		client.printf("GET /fetch?train=%c&stop-id=%s&n-results=2 HTTP/1.0\r\n",
-				page->train, page->stop_id);
-		client.printf("Host: %s\r\n", server);
-		client.println();
-	} else {
-		Serial.println("[DATA] Request failed");
+	if (make_request(page)) {
+		Serial.printf("[REQUEST] Failed to make request for %c trains at %s\n",
+				page.train, page.name);
 		return;
 	}
-	Serial.println("[DATA] Finished API request");
 
-	while (!client.available()) {
-		delay(100);
+	if (read_response(page)) {
+		Serial.printf("[RESPONSE] Failed to read response for %c trains at %s\n",
+				page.train, page.name);
+		return;
 	}
-
-	while (client.available() && bytes_read < sizeof(buf)) {
-		char c = client.read();
-		buf[bytes_read++] = c;
-	}
-
-	buf[bytes_read] = 0;
-	client.stop();
-
-	ptr = ptr % NPAGES;
-
-	Serial.printf("[DATA]\n%s\n", buf);
 }
